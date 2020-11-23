caf9a1414899aac68d26d5eae1edc3684ce4e811
keerah <9025818+keerah@users.noreply.github.com>
2020-11-23 07:43:48 +0300

    Coding progress
    Added Import and basic compositing

--- RSpass2AE.jsx
+++ RSpass2AE.jsx
@@ -5,10 +5,11 @@
 
     var RSpass2AE =  new Object;
         RSpass2AE.scriptName = "RS passes to AE";
-        RSpass2AE.version = "0.11";
+        RSpass2AE.version = "0.25";
         RSpass2AE.AOVtag = "_AOV_";
+        RSpass2AE.homeFolderName = "RS render passes";
 
-    var PassList = [{name: "Beauty", tag: "Beauty", type: 0, light: false},
+    var passList = [{name: "Beauty", tag: "Beauty", type: 0, light: false},
                     {name: "Diffuse Lighting", tag: "Diffuse", type: 0, light: true}, 
                     {name: "Specular Lighting", tag: "Speculars", type: 0, light: true},
                     {name: "Reflections", tag: "Reflections", type: 0, light: true},
@@ -45,16 +46,20 @@
                     {name: "Ambient Occusion", tag: "AO", type: 3, light: false}]
 
 
-    var PassType =  ["Standard Shading AOV",
+    var passType =  ["Standard Shading AOV",
                     "Raw Shading AOV",
                     "Utility AOV",
                     "Deprecated AOV"];
 
-    var SequenceData = [];
-
-    $.writeln("\r--- new run inititated ---\r")
+    var sequenceData = [], baseName = null, homeFolder = null;
 
 
+
+    function exDelay(msec, looped) {
+        
+        app.scheduleTask("app.project.showWindow(true); app.activate();", msec, looped);
+        
+    }
 
     function FilterFilesExt(arg) {
 
@@ -71,207 +76,305 @@
 
 
 
-    function GetFiles(FolderName) {
-
-
-        var SourceFolder = new Folder(FolderName), MyFiles = [[],];
-
-        if ((SourceFolder.exists) && (SourceFolder instanceof Folder)) {
-
-            try { 
-
-                $.writeln("The folder " + SourceFolder.name + " assigned");
-                MyFiles = SourceFolder.getFiles(FilterFilesExt).sort();
-                
-            } catch(err) {
-
-                $.writeln("Some filesystem error: " + SourceFolder.error);
-                alert(err.toString());
-                return null
-            }   
-        } else {
-
-            alert("The specified folder was not found", RSpass2AE.scriptName);
-            return null
-        } // if folder exists
-
-        if (MyFiles.length > 0) {
+    function getProjItem(proj, itemName) {
         
-            $.writeln("Total Files: " + MyFiles.length);
-            return MyFiles
-
-        } else {
-
-            alert("There were no sequence files found in the folder", RSpass2AE.scriptName)
-            return null
-        } // if MyFiles has files
-    } // getfiles()
-
-
-    function GetBaseName(FolderName) {
-
-
-        var BaseName = null, SourceFiles = GetFiles(FolderName);
-        var AOVTagPos = -1, CounterPos = -1;
+        //search for first matching name project item 
         
-            if (SourceFiles)  {
-        
-                // parse files for BaseName 
-                
-                for (var i = 0; i < SourceFiles.length; i++) {
-                    
-                    AOVTagPos = SourceFiles[i].name.indexOf(RSpass2AE.AOVtag); // aov tag not present
-                    if (AOVTagPos != -1) {
+        for (var i = 1; i <= proj.numItems; i++) {
 
-                        BaseName = SourceFiles[i].name.substring(0, AOVTagPos);
-                        $.writeln("BaseName from no AOV tag filename: " + BaseName);
-                    } 
-
-                    CounterPos = SourceFiles[i].name.search("\\d{4,}"); // the counter present
-                    if (CounterPos != -1) {
-
-                        if (BaseName != null) { // and AOVtag was found too
-
-                            $.writeln("No AOV tag in this filename. This must be the main beauty pass");
-                            break
-
-                        } else { // no AOV tag found, getting the new BaseName or overwriting (should be the same)
-
-                            BaseName = SourceFiles[i].name.substring(0, CounterPos);
-                            $.writeln("BaseName from AOV tagged filename: " + BaseName);
-                            break
-                        }
-
-                    } else {
-
-                        break
-                    }
-                } // for
-            }// if theres files
-            
-        return BaseName
+            if (proj.item(i).name == itemName) {
+                return proj.item(i);
+                break
+            }
+        }
 
     }
 
 
 
-    function GetData(FolderName) {
+    function GetFiles(sourceFolder) {
 
 
-        var BaseName = GetBaseName(FolderName), SourceFiles = GetFiles(FolderName);
-        var CounterPos = -1;
+        var myFiles = [];
 
-        if ((BaseName != null) & (SourceFiles.length > 0)) {
+        if ((sourceFolder.exists) && (sourceFolder instanceof Folder)) {
+
+            try { 
+
+                $.writeln("The folder " + sourceFolder.name + " assigned");
+                myFiles = sourceFolder.getFiles(FilterFilesExt).sort();
+                
+            } catch(err) {
+
+                $.writeln("Some filesystem error: " + sourceFolder.error);
+                alert(err.toString());
+                return 0
+            }   
+        } else {
+
+            alert("The specified folder was not found", RSpass2AE.scriptName);
+            return 0
+        } // if folder exists
+
+        if (myFiles.length > 0) {
+        
+            $.writeln("Total Files: " + myFiles.length);
+            return myFiles
+
+        } else {
+
+            alert("There were no sequence files found in the folder", RSpass2AE.scriptName)
+            return 0
+        } // if myFiles has files
+    } // getfiles()
+
+
+    function getBaseName(sourceFolder) {
+
+
+        var sourceFiles = GetFiles(sourceFolder);
+        var aovTagPos = -1, counterPos = -1;
+
+        if (sourceFiles.length > 0)  {
+
+            baseName = null; // reset global baseName
+            
+            for (var i = 0; i < sourceFiles.length; i++) {
+                
+                aovTagPos = sourceFiles[i].name.indexOf(RSpass2AE.AOVtag); // aov tag not present
+                if (aovTagPos != -1) {
+
+                    baseName = sourceFiles[i].name.substring(0, aovTagPos);
+                    $.writeln("baseName from no AOV tag filename: " + baseName);
+                } 
+
+                counterPos = sourceFiles[i].name.search("\\d{4,}"); // the counter present
+                if (counterPos != -1) {
+
+                    if (baseName != null) { // and AOVtag was found too
+
+                        $.writeln("No AOV tag in this filename. This must be the main beauty pass");
+                        break
+
+                    } else { // no AOV tag found, getting the new baseName or overwriting (should be the same)
+
+                        baseName = sourceFiles[i].name.substring(0, counterPos);
+                        $.writeln("baseName from AOV tagged filename: " + baseName);
+                        break
+                    }
+
+                } else {
+
+                    break
+                }
+            } // for
+
+            exDelay(100, 0);
+        }// if theres files
+    } // getBasename
+
+
+
+    function GetData(sourceFolder) {
+
+        var counterPos = -1, sourceFiles = GetFiles(sourceFolder);
+
+        if ((baseName != null) & (sourceFiles.length > 0)) {
 
             // 1 collecting the sequences
             // 1a strip filenames to pass tags storing to the equal array
-            var StrippedNames = [], AfterNamePos = BaseName.length, AOVPos;
+            var strippedNames = [], afterNamePos = baseName.length, aovPos;
 
-            for (var i = 0; i < SourceFiles.length; i++) {
+            for (var i = 0; i < sourceFiles.length; i++) {
 
-                CounterPos = SourceFiles[i].name.search("\\d{4,}");
-                if (CounterPos == -1) CounterPos = SourceFiles[i].name.length;
+                counterPos = sourceFiles[i].name.search("\\d{4,}");
+                if (counterPos == -1) counterPos = sourceFiles[i].name.length;
                 
-                // checking for AOV tag presence right next to the BaseName
-                AOVPos = SourceFiles[i].name.search(RSpass2AE.AOVtag);
-                if (AOVPos != -1) AfterNamePos = AOVPos + RSpass2AE.AOVtag.length;
-                StrippedNames[i] = SourceFiles[i].name.substring(AfterNamePos, CounterPos);
+                // checking for AOV tag presence right next to the baseName
+                aovPos = sourceFiles[i].name.search(RSpass2AE.AOVtag);
+                if (aovPos != -1) afterNamePos = aovPos + RSpass2AE.AOVtag.length;
+                strippedNames[i] = sourceFiles[i].name.substring(afterNamePos, counterPos);
             }
 
             // 1b selecting each new stripped name and adding to the list of uniques
-            var UniSequences = [];
-            UniSequences.push(0);
-            $.writeln("Fisrt sequence added: " + StrippedNames[0]);
+            var uniSequences = [];
+            uniSequences.push(0);
+            $.writeln("Fisrt sequence added: " + strippedNames[0]);
 
-            for (var f = 1; f < SourceFiles.length; f++) {
+            for (var f = 1; f < sourceFiles.length; f++) {
         
-                if (StrippedNames[f-1] != StrippedNames[f]) {
-                    UniSequences.push(f);
-                    $.writeln("New sequence found and added: " + StrippedNames[f]);
+                if (strippedNames[f-1] != strippedNames[f]) {
+                    uniSequences.push(f);
+                    $.writeln("New sequence found and added: " + strippedNames[f]);
                     f++;
                 }
             }
 
-            $.writeln("Total unique sequences found: " + UniSequences.length);
+            $.writeln("Total unique sequences found: " + uniSequences.length);
 
             // 1c classifing the sequences
-            var CFlag = false; CCount = 0;
+            var cFlag = false; cCount = 0;
 
-            SequenceData = [];
+            sequenceData = [];
 
-            for (var s = 0; s < UniSequences.length; s++) {
+            for (var s = 0; s < uniSequences.length; s++) {
 
-                CFlag = false;
+                cFlag = false;
 
-                if (StrippedNames[UniSequences[s]] == "") { // considered default Beauty pass
+                if (strippedNames[uniSequences[s]] == "") { // considered default Beauty pass
 
-                    SequenceData.push({id: UniSequences[s], PassId: 0, light: 0});
-                    CFlag = true; CCount++;
-                    $.writeln(SourceFiles[UniSequences[s]].name + " is " + PassList[0].name);
+                    sequenceData.push({fileid: uniSequences[s], file: sourceFiles[uniSequences[s]], passid: 0, passname: passList[0].name, lightid: -1, lightname: 0});
+                    cFlag = true; cCount++;
+                    $.writeln(sourceFiles[uniSequences[s]].name + " is " + passList[0].name);
 
                 } else { 
 
-                    for (var p = 0; p < PassList.length; p++) {
+                    for (var p = 0; p < passList.length; p++) {
 
-                        if (StrippedNames[UniSequences[s]].indexOf(PassList[p].tag) != -1) {
+                        if (strippedNames[uniSequences[s]].indexOf(passList[p].tag) != -1) {
 
-                            SequenceData.push({id: UniSequences[s], PassId: p, light: 0});
-                            CFlag = true; CCount++;
-                            $.writeln(SourceFiles[UniSequences[s]].name + " is " + PassList[p].name);
+                            sequenceData.push({fileid: uniSequences[s], file: sourceFiles[uniSequences[s]], passid: p, passname: passList[p].name, lightid: -1, lightname: 0});
+                            cFlag = true; cCount++;
+                            $.writeln(sourceFiles[uniSequences[s]].name + " is " + passList[p].name);
                             break;
                         }
                     }
                 }
 
-                if (!CFlag) {
+                if (!cFlag) {
 
-                    $.writeln(SourceFiles[UniSequences[s]].name + " was not classified. Skipping!")
+                    $.writeln(sourceFiles[uniSequences[s]].name + " was not classified. Skipping!")
                 }
             }
 
-            $.writeln(CCount + " of " + SequenceData.length + " passes were classified");
-
+            $.writeln(cCount + " of " + sequenceData.length + " passes were classified");
 
             // 2 Ligtpass groups processing, if any
 
             var LightGroup = [], lp;
 
-            for (var s = 0; s < SequenceData.length; s++) {
+            for (var s = 0; s < sequenceData.length; s++) {
 
-                lp = StrippedNames[SequenceData[s].id].split("_");
+                lp = strippedNames[sequenceData[s].fileid].split("_");
                 if (lp[1] != undefined) {
 
                     LightGroup.push(lp[1]);
-                    SequenceData[s].light = LightGroup.length - 1;
-                    $.writeln(StrippedNames[SequenceData[s].id] + " assigned to lightgroup " + lp[1]);
+                    sequenceData[s].lightid = LightGroup.length - 1;
+                    sequenceData[s].lightname = lp[1];
+                    $.writeln(strippedNames[sequenceData[s].fileid] + " assigned to lightgroup " + lp[1]);
                 }
             }
 
             $.writeln(LightGroup.length + " lightgroups detected");
 
-        } // if BaseName
+        } // if baseName
         
-        return SequenceData    
+        return sequenceData    
     } // GetData
     
     
 
-    function ImportData(SequenceData) {
+    function importData(sequenceData, FPS) {
 
         
-        var Proj = app.thisproject, Home = (Proj.activeItem.typeName == "Folder") ? Proj.activeItem : Proj.root
-
-        var importOptions = new ImportOptions(files[index]);
-        importOptions.sequence = true;
+        var currentProj = app.project, iCount = 0;
         
-        var importedFile = Proj.importFile(importOptions);
-		importedFile.parentFolder = selectedFolder;
-		importedFile.mainSource.conformFrameRate = FPS;
+        if ((currentProj) && (sequenceData.length > 0)) {
+        
+            $.writeln("Now Importing...");
+            var importOptions, importedFile;
+            var importHome = currentProj.rootFolder, lightFolders=[];
+            if (currentProj.activeItem != null) {
+
+                importHome = (currentProj.activeItem.typeName == "Folder") ? currentProj.activeItem : currentProj.rootFolder
+            }
+
+            homeFolder = currentProj.items.addFolder(RSpass2AE.homeFolderName);
+            homeFolder.parentFolder = importHome;
+            var setFolder = currentProj.items.addFolder(baseName + " Footage");
+            setFolder.parentFolder = homeFolder;
+            
+            for (var f = 0; f < sequenceData.length; f++) {
+                
+                importOptions = new ImportOptions(sequenceData[f].file);
+                importOptions.sequence = true;
+                
+                var importedFile = currentProj.importFile(importOptions);
+                importedFile.mainSource.conformFrameRate = FPS;
+                sequenceData[f].footage = importedFile;
+
+                if (sequenceData[f].lightid != -1) { // if the footage in a lightgroup
+
+                    if (lightFolders[sequenceData[f].lightid] == undefined ) { // look if this lightgroup has a folder
+
+                        lightFolders[sequenceData[f].lightid] = currentProj.items.addFolder(sequenceData[f].lightname);
+
+                    }
+
+                    importedFile.parentFolder = lightFolders[sequenceData[f].lightid]; 
+                    lightFolders[sequenceData[f].lightid].parentFolder = setFolder;
+
+                } else { 
+                    
+                    importedFile.parentFolder = setFolder
+                }
+
+                iCount++
+            }
+
+        } // if current project and data
+
+        return iCount 
+
+    } // importData
 
 
 
-    } // ImportData function
+    function autoComp(sequenceData) {
 
+        var currentProj = app.project, cCount = 0, newComps = [];
+
+        if ((currentProj) && (sequenceData.length > 0) && (homeFolder != null)) {
+
+            var tItem = sequenceData[0].footage, tLayer;
+            compFolder = currentProj.items.addFolder(baseName + " Auto Compositions");
+            compFolder.parentFolder = homeFolder;
+
+            //create default comp
+            var defComp = currentProj.items.addComp("Main", tItem.width, tItem.height, tItem.pixelAspect, tItem.duration, tItem.frameRate);
+            defComp.parentFolder = compFolder;
+
+            for (var s = 0; s < sequenceData.length; s++) {
+
+                if (sequenceData[s].lightid != -1) { // if the footage in a lightgroup
+                    
+                    if (newComps[sequenceData[s].lightid] == undefined ) { // look if this lightgroup has a comp already
+
+                        newComps[sequenceData[s].lightid] = currentProj.items.addComp(sequenceData[s].lightname, tItem.width, tItem.height, tItem.pixelAspect, tItem.duration, tItem.frameRate);
+                        newComps[sequenceData[s].lightid].parentFolder = compFolder;
+
+                    }
+
+                    // adding layers
+                    tLayer = newComps[sequenceData[s].lightid].layers.add(sequenceData[s].footage);
+                    tLayer.blendingMode = BlendingMode.ADD;
+                    sequenceData[s].layer = tLayer;
+
+                } else { // to main comp
+
+                    tLayer = defComp.layers.add(sequenceData[s].footage);
+                    tLayer.blendingMode = BlendingMode.ADD;
+                    sequenceData[s].layer = tLayer;
+
+                }
+
+            }
+
+            defComp.openInViewer;
+
+        } // if current project and data
+
+    }
 
 
     function buildUI(thisObj){
@@ -289,8 +392,10 @@
     } // buildUI
 
 
-    // main
+    ////////////////// main
     
+    $.writeln("\r--- new run inititated ---\r");
+
     // var trToolPal = buildUI(thisObj);
 
     // if (trToolPal !== null)
@@ -306,10 +411,35 @@
     //         }
 
 
-    var MyFolder = "D:\\keerah\\PROJECTS\\00\\Front1\\";
-    var SeqData = GetData(MyFolder);
-    
-    return (SeqData) ? SeqData.length : "nothing's done"
+    //var myFolder = "D:\\keerah\\PROJECTS\\00\\ForestZoom1\\"; 
 
+    var myFolder = Folder.selectDialog("Select the renderpass folder");
+    if (myFolder != null) {
 
+        getBaseName(myFolder);
+
+        if (baseName != null) {
+
+            var seqData = GetData(myFolder); 
+
+            if ((seqData) && (seqData.length > 0)) {
+
+                var filesImported = importData(seqData, 25);
+                $.writeln (filesImported + " imported");
+
+                if (filesImported > 0) {
+
+                    var compCreated = autoComp(seqData)
+                }
+
+            } else {
+            
+                //
+            }
+        
+        } //basename check
+
+    } // folder check
+
+    $.writeln((seqData) ? seqData.length : ("sequences found, and " + filesImported + " imported"))
 })(this);
